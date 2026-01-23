23ca4aecf392a7d29dad6d6f33e8c14f7494fb28
BoySyntax <teroaaron23@gmail.com>
2026-01-23 17:01:57 +0800

    Fix Vercel routes for budget/category pages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
         index: resolve(__dirname, "index.html"),
         app: resolve(__dirname, "app.html"),
         login: resolve(__dirname, "login.html"),
+        budget: resolve(__dirname, "budget.html"),
+        category: resolve(__dirname, "category.html"),
       },
     },
   },
